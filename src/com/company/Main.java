6298209409f1d8b4c3ef2a80263cbd01d78cb628
Abel Mescua <abelmescua@gmail.com>
2020-06-13 12:37:41 +0200

    calculator challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // take two values, divide one by the other.
+        // both must be double
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        double n1 = scanner.nextDouble();
+        System.out.print("Enter a numeric value: ");
+        double n2 = scanner.nextDouble();
+
+        double answer = n1 / n2;
+        System.out.println("The answer is " + answer);
     }
 }
