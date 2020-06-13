2e9c5983491e7142d806614bdb0284b5a2982be1
Abel Mescua <abelmescua@gmail.com>
2020-06-13 13:01:15 +0200

    switch statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,20 @@ public class Main {
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
 
+        switch(monthNumber) {
+            case 1:
+                System.out.println("The month is January");
+                break;
+            case 2:
+                System.out.println("The month is February");
+                break;
+            case 3:
+                System.out.println("The month is March");
+                break;
+            case 4:
+                System.out.println("The month is April");
+            default:
+                System.out.println("You choose another month");
+        }
     }
 }
