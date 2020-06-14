dbc6a75ad1157e7ce73b4a4a7c9d96a0461d6168
Abel Mescua <abelmescua@gmail.com>
2020-06-14 12:56:48 +0200

    close objects with try catch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,17 @@ public class Main {
     public static void main(String[] args) {
         var file = new File("hello.txt");
         System.out.println("File exists: " + file.exists());
+
+        // objects in () will be close automatically after their use (to evade lick memory)
+        // objects that needs to be close, have the interface "Closeable" in their implementation
+        try (FileReader reader = new FileReader(file);
+             BufferedReader br = new BufferedReader(reader)) {
+
+            var text = br.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
